462b997e8b12358c3e058d2da33a8ab144e7c339
Buddhika sepali Rathnayaka <buddhirathnayaka@gmail.com>
2024-10-06 11:10:02 +0530

    modifications

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-
-
-function App() {
+import { toHaveFormValues } from "@testing-library/jest-dom/matcher"
+const valueprinter = ({name})=>
+function App(value) {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{value} </p>
     </div>
   );
 }
